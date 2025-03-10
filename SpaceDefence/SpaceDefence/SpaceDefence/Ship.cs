f726567ea4e57688831e50d984b6e3d83dfea59a
Kelvin de Looff <kelvindelooff@gmail.com>
2025-03-10 17:33:06 +0100

    Let's the user screen warp and fixed the 360 degree rotation

--- SpaceDefence/SpaceDefence/SpaceDefence/Ship.cs
+++ SpaceDefence/SpaceDefence/SpaceDefence/Ship.cs
@@ -46,11 +46,9 @@ namespace SpaceDefence
             base_turret = content.Load<Texture2D>("base_turret");
             laser_turret = content.Load<Texture2D>("laser_turret");
             _rectangleCollider.shape.Size = ship_body.Bounds.Size;
-            _rectangleCollider.shape.Location -= new Point(ship_body.Width/2, ship_body.Height/2);
+            _rectangleCollider.shape.Location -= new Point(ship_body.Width / 2, ship_body.Height / 2);
             base.Load(content);
         }
-
-
 
         public override void HandleInput(InputManager inputManager)
         {
@@ -73,13 +71,10 @@ namespace SpaceDefence
             {
                 acceleration.Normalize();
                 acceleration *= accelerationSpeed;
-
-                rotation = (float)Math.Atan2(acceleration.X, -acceleration.Y);
             }
 
             if (inputManager.LeftMousePress())
             {
-
                 Vector2 aimDirection = LinePieceCollider.GetDirection(GetPosition().Center, target);
                 Vector2 turretExit = _rectangleCollider.shape.Center.ToVector2() + aimDirection * base_turret.Height / 2f;
                 if (buffTimer <= 0)
@@ -88,7 +83,7 @@ namespace SpaceDefence
                 }
                 else
                 {
-                    GameManager.GetGameManager().AddGameObject(new Laser(new LinePieceCollider(turretExit, target.ToVector2()),400));
+                    GameManager.GetGameManager().AddGameObject(new Laser(new LinePieceCollider(turretExit, target.ToVector2()), 400));
                 }
             }
         }
@@ -116,11 +111,35 @@ namespace SpaceDefence
             // Update position based on velocity
             _rectangleCollider.shape.Location += velocity.ToPoint();
 
+            // Update rotation based on velocity direction
+            if (velocity != Vector2.Zero)
+            {
+                rotation = (float)Math.Atan2(velocity.Y, velocity.X) + MathHelper.PiOver2;
+            }
+
+            // Handle screen wrapping
+            HandleScreenWrapping();
+
             // Update the Buff timer
             if (buffTimer > 0)
                 buffTimer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             base.Update(gameTime);
+        }
+
+        private void HandleScreenWrapping()
+        {
+            Rectangle viewport = GameManager.GetGameManager().Game.GraphicsDevice.Viewport.Bounds;
+
+            if (_rectangleCollider.shape.Right < 0)
+                _rectangleCollider.shape.X = viewport.Width;
+            else if (_rectangleCollider.shape.Left > viewport.Width)
+                _rectangleCollider.shape.X = -_rectangleCollider.shape.Width;
+
+            if (_rectangleCollider.shape.Bottom < 0)
+                _rectangleCollider.shape.Y = viewport.Height;
+            else if (_rectangleCollider.shape.Top > viewport.Height)
+                _rectangleCollider.shape.Y = -_rectangleCollider.shape.Height;
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
@@ -153,7 +172,6 @@ namespace SpaceDefence
             }
             base.Draw(gameTime, spriteBatch);
         }
-
 
         public void Buff()
         {
